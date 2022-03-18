b7a32789f2eb74944534e4742cb91bf8b4d93ec5
trand27 <trand27@PF2XTKM5.wit.private>
2022-03-18 10:52:50 -0400

    started working on menu

--- src/application/Menu.java
+++ src/application/Menu.java
@@ -1,10 +1,30 @@
 package application;
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.stage.*; 
+import javafx.scene.layout.*; 
+import javafx.scene.control.*;
+import javafx.event.*; 
+import javafx.geometry.*;
 
-public class Menu {
+public class Menu extends Main{
 
+	Label response;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		launch(args);
 
+	}
+	
+	public void start(Stage myStage) {
+		myStage.setTitle("Ttris");
+		
+		BorderPane rootNode = new BorderPane();
+		
+		Scene myScene = new Scene(rootNode, 300, 300);
+		
+		 myStage.setScene(myScene);
 	}
 
 }
